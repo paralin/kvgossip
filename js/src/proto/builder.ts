0d01eb450a82282b30e9847cdefc30929272bbe1
Christian Stewart <christian@paral.in>
2016-12-19 12:11:11 -0800

    Revert "feat: basic js project structure"
    This reverts commit 779f7d9552bc20e7340fe8840e28bbdaa6f4805b.

--- js/src/proto/builder.ts
+++
@@ -1,5 +0,0 @@
-import { PROTO_DEFINITIONS } from './definitions';
-
-import * as ProtoBuf from 'protobufjs';
-
-export const ProtoRoot = ProtoBuf.Root.fromJSON(PROTO_DEFINITIONS);
--- js/src/proto/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './interfaces';
-export * from './definitions';
-export * from './builder';
