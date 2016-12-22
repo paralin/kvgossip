9e910c8fc27a13febc4e267cf8bd59617ed4262f
Christian Stewart <christian@paral.in>
2016-12-21 16:03:42 -0800

    refactor: split cmds into separate package

--- cmd/kvgossip/main.go
+++ cmd/kvgossip/main.go
@@ -4,31 +4,10 @@ import (
 	"os"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/fuserobotics/kvgossip/cmd/kvgossip/cmds"
 	ver "github.com/fuserobotics/kvgossip/version"
 	"github.com/urfave/cli"
 )
-
-var RootCommands = []cli.Command{
-	AgentCommand,
-	ManualSyncCommand,
-	KeyGenCommand,
-	ControlCommand,
-}
-
-var RootFlags = []cli.Flag{
-	cli.StringFlag{
-		Name:        "dbpath, d",
-		Usage:       "Use database at `PATH`.",
-		Value:       "./kvgossip.db",
-		Destination: &AgentFlags.DbPath,
-	},
-	cli.StringFlag{
-		Name:        "rootkey, r",
-		Usage:       "Use root key at `PATH`.",
-		Value:       "./root_key.pem",
-		Destination: &AgentFlags.RootKeyPath,
-	},
-}
 
 func main() {
 	log.SetLevel(log.DebugLevel)
@@ -37,7 +16,7 @@ func main() {
 	app.Name = "kvgossip"
 	app.Version = ver.KVGossipVersion
 	app.EnableBashCompletion = true
-	app.Commands = RootCommands
-	app.Flags = RootFlags
+	app.Commands = cmds.RootCommands
+	app.Flags = cmds.RootFlags
 	app.Run(os.Args)
 }
